d82cc2c881141de21e5b979b333c65c668f70e69
Benzazoua <billalben252@gmail.com>
2024-06-13 18:56:53 +0100

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const imageBaseUrl = "https://image.tmdb.org/t/p/";
-const base_url = "http://127.0.0.1:3000";
+const base_url = "https://tvflex-movie.netlify.app";
+// const base_url = "http://127.0.0.1:3000";
 
 const fetchDataFromServer = async (url, callback, optionalParams) => {
   try {
